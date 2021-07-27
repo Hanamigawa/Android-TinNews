d35ecc91e198240d91d1610374d13a251c758a9b
Hanamigawa <zwhether@gmail.com>
2021-07-27 16:02:41 -0400

    implement MVVM

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -5,19 +5,23 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
 import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel viewModel;
     public HomeFragment() {
         // Required empty public constructor
+
     }
 
 
@@ -32,6 +36,18 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         NewsRepository repository = new NewsRepository();
-        viewModel = new HomeViewModel(repository);
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
+                .get(HomeViewModel.class);
+        viewModel.setCountryInput("us");
+        viewModel
+                .getTopHeadlines()
+                .observe(
+                        getViewLifecycleOwner(),
+                        newsResponse -> {
+                            if (newsResponse != null) {
+                                Log.d("HomeFragment", newsResponse.toString());
+                            }
+                        });
+
     }
 }
