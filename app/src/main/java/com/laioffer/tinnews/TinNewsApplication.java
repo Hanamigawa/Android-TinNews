1a6dd83f9db8fda76309ac2c8a63ffc9b6391c09
Hanamigawa <zwhether@gmail.com>
2021-07-27 15:12:13 -0400

    initialized

---
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -0,0 +1,11 @@
+package com.laioffer.tinnews;
+
+import android.app.Application;
+
+public class TinNewsApplication extends Application {
+
+//    @Override
+//    public void OnCreate() {
+//        super.onCreate();
+//    }
+}
---
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -0,0 +1,37 @@
+package com.laioffer.tinnews.ui.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.repository.NewsRepository;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel viewModel;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NewsRepository repository = new NewsRepository();
+        viewModel = new HomeViewModel(repository);
+    }
+}
